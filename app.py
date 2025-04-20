7579fec66d50be4d5f65520fcdc8ab4ea9f4aa90
Rahul Sharma <rahul.sharma2@ing.com>
2025-04-20 20:50:50 +0530

    Updated code

--- app.py
+++ app.py
@@ -19,6 +19,15 @@ def greet():
    conn.commit()
    conn.close()
    return f"<h2>Hello, {name}!</h2><br><a href='/'>Go back</a>"
+
+@app.route('/all')
+def all_greetings():
+   conn = sqlite3.connect('data.db')
+   c = conn.cursor()
+   c.execute('SELECT name FROM greetings')
+   names = c.fetchall()
+   conn.close()
+   return "<br>".join(name[0] for name in names)
    
 if __name__ == '__main__':
    init_db()
